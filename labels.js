04a907c7741492cdb5efe61f1111803c97a5673b
jhssong <jhssong02@gmail.com>
2024-12-26 01:11:50 +0900

    Implement automatically add predefined labels function

---
+++ labels.js
@@ -0,0 +1,69 @@
+const labels = [
+  {
+    name: "⚙ Setting",
+    color: "e3dede",
+    description: "개발 환경 세팅",
+  },
+  {
+    name: "✨ Feature",
+    color: "a2eeef",
+    description: "기능 및 개선사항",
+  },
+  {
+    name: "🌏 Deploy",
+    color: "C2E0C6",
+    description: "배포 관련",
+  },
+  {
+    name: "🎨Html&Css",
+    color: "FEF2C0",
+    description: "마크업 & 스타일링(UI)",
+  },
+  {
+    name: "🐞 BugFix",
+    color: "d73a4a",
+    description: "버그 제보 및 수정",
+  },
+  {
+    name: "💻 CrossBrowsing",
+    color: "C5DEF5",
+    description: "브라우저 호환성",
+  },
+  {
+    name: "📃 Docs",
+    color: "1D76DB",
+    description: "문서 작성 및 수정 (README.md 등)",
+  },
+  {
+    name: "📬 API",
+    color: "D4C5F9",
+    description: "서버 API 통신",
+  },
+  {
+    name: "🧹 CleanUp",
+    color: "FEF2C0",
+    description: "코드를 깔끔하게 수정",
+  },
+  {
+    name: "🔨 Refactor",
+    color: "f29a4e",
+    description: "코드 리팩토링 (작동 방식 변경)",
+  },
+  {
+    name: "🙋‍♂️ Question",
+    color: "9ED447",
+    description: "질문만 있는 이슈",
+  },
+  {
+    name: "🥰 Accessibility",
+    color: "facfcf",
+    description: "웹접근성 관련",
+  },
+  {
+    name: "✅ Test",
+    color: "ccffc4",
+    description: "test 관련",
+  },
+];
+
+export default labels;
