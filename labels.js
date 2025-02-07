71aeb4d2e75f0fd40bc9e1e83fc8f571ff06219b
jhssong <jhssong02@gmail.com>
2025-02-08 00:15:56 +0900

    Remove unusing labels

--- labels.js
+++ labels.js
@@ -1,68 +1,48 @@
 const labels = [
   {
-    name: "⚙ Setting",
-    color: "e3dede",
-    description: "개발 환경 세팅",
-  },
-  {
     name: "✨ Feature",
     color: "a2eeef",
-    description: "기능 및 개선사항",
-  },
-  {
-    name: "🌏 Deploy",
-    color: "C2E0C6",
-    description: "배포 관련",
-  },
-  {
-    name: "🎨Html&Css",
-    color: "FEF2C0",
-    description: "마크업 & 스타일링(UI)",
+    description: "Features and improvements",
   },
   {
     name: "🐞 BugFix",
     color: "d73a4a",
-    description: "버그 제보 및 수정",
-  },
-  {
-    name: "💻 CrossBrowsing",
-    color: "C5DEF5",
-    description: "브라우저 호환성",
-  },
-  {
-    name: "📃 Docs",
-    color: "1D76DB",
-    description: "문서 작성 및 수정 (README.md 등)",
-  },
-  {
-    name: "📬 API",
-    color: "D4C5F9",
-    description: "서버 API 통신",
-  },
-  {
-    name: "🧹 CleanUp",
-    color: "FEF2C0",
-    description: "코드를 깔끔하게 수정",
+    description: "Bug reports and fixes",
   },
   {
     name: "🔨 Refactor",
     color: "f29a4e",
-    description: "코드 리팩토링 (작동 방식 변경)",
+    description: "Code refactoring (changes in functionality)",
+  },
+  {
+    name: "📃 Docs",
+    color: "1D76DB",
+    description: "Writing and editing documentation",
+  },
+  {
+    name: "⚙ Setting",
+    color: "e3dede",
+    description: "Development environment setup",
+  },
+  {
+    name: "🌏 Deploy",
+    color: "C2E0C6",
+    description: "Deployment-related tasks",
+  },
+  {
+    name: "🧹 CleanUp",
+    color: "FEF2C0",
+    description: "Cleaning up and organizing the code",
   },
   {
     name: "🙋‍♂️ Question",
     color: "9ED447",
-    description: "질문만 있는 이슈",
-  },
-  {
-    name: "🥰 Accessibility",
-    color: "facfcf",
-    description: "웹접근성 관련",
+    description: "Issues with only questions",
   },
   {
     name: "✅ Test",
     color: "ccffc4",
-    description: "test 관련",
+    description: "Test-related tasks",
   },
 ];
 
